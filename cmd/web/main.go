800db684b7274724c47e1f5aa96675309ab231c3
mayura-andrew <mayuraalahakoon@gmail.com>
2023-12-13 02:05:36 +0530

    creating a database connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 )
 
-
-// define an application struct to hold the application wide dependencies for the 
-// web application. For now we'll only include fields for the two custom logger 
+// define an application struct to hold the application wide dependencies for the
+// web application. For now we'll only include fields for the two custom logger
 // we'll add more to it as the build progresses.
 
 type application struct {
@@ -24,6 +26,12 @@ func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
 
+
+	// Define a new command-line flag for the MySQL DNS string
+	//fmt.Printf("web:pass@tcp(localhost:3306)/snippetbox?parseTime=true")
+
+	dsn := flag.String("dsn", "root:mypass@/snippetbox?parseTime=true", "MySQL database")
+
 	// Importantly, we use the flag.Parse() function to parse the command-line
 	// This reads in the command-line flag value and assigns it to the addr
 	// variable. You need to call this *before* you use the addr variable
@@ -32,12 +40,17 @@ func main() {
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
 
 
 	// initialize a new instance of application containing the dependencies.
 
+	defer db.Close()
 	app := &application{
 		errorLog: errorLog,
 		infoLog: infoLog,
@@ -70,7 +83,22 @@ func main() {
 		Handler: app.routes(), // call the new app.routes() method
 	}
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 
+}
+
+// the openDB() function wraps sql.Open() and returns a sql.DB connection pool
+// for a diven DSN
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
